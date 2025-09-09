3c4f5b8562ae805dcfa58e5ed0637b07cb7be2ba
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-09 18:26:49 +0100

    Promieres tantations

--- main.c
+++ main.c
@@ -2,6 +2,44 @@
 
 
 
+struct Joueur{
+    char Id[4];
+    char nom[10];
+    int numeroMaillot;
+    char poste[10];
+    int age;
+    int buts;
+};
+
+int nombre_joueurs = 0;
+struct Joueur joueurs[nombre_joueurs];
+
+void ajouter(){
+    printf("Ajouter un joueur\n");
+    printf("1: Ajouter un nouveau joueur.\n");
+    printf("2: Ajouter plusieurs joueurs en une seule opération.\n");
+}
+
+void afficher(){
+
+}
+
+void modifier(){
+
+}
+
+void supprimer(){
+
+}
+
+void rechercher(){
+
+}
+
+void Stat(){
+
+}
+
 int main(){
 
 
